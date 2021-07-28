cf39193fdc6bdecd61528dd360bd986e8c40aef3
Elmir Ismayilzade <devkryternext@gmail.com>
2021-07-28 19:28:55 +0400

    chore: reformat index.ts file

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
-import express, {Application, Request, Response} from "express"
+import express, { Application, Request, Response } from 'express';
 
-const app: Application = express()
+const app: Application = express();
 
-const port = 3000
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.get('/', async(req: Request, res:Response): Promise<Response> => {
-  return res.status(200).send({message: `Welcome to the cookbook API! \n Endpoints available at http://localhost:${port}/api/v1`})
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({ message: `Welcome to the cookbook API! \n Endpoints available at http://localhost:${port}/api/v1` });
+});
 
 try {
-    app.listen(port, ()=>{
-        console.log(`Server running on http://localhost:${port}`)
-    })
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 } catch (err) {
-    console.error(`Error occurred: ${err.message}`)
+  console.error(`Error occurred: ${err.message}`);
 }
